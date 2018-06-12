b6eb5295a34218c059a438f74135ec4573b76961
Mo <medsriha@live.fr>
2018-06-11 22:19:06 -0400

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -2,23 +2,18 @@ import cv2
 import glob as gb
 import random
 import numpy as np
-
-# Emotion list
-emojis = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
-# Initialize fisher face classifier
-fisher_face = cv2.face.FisherFaceRecognizer_create()
+#Emotion list
+emojis = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] 
+ #Initialize fisher face classifier
+fisher_face = cv2.createFisherFaceRecognizer()
 data = {}
-
-
-# Function defination to get file list, randomly shuffle it and split 67/33
-def getFiles(emotion):
-    files = gb.glob("final_dataset\\%s\\*" % emotion)
+#Function defination to get file list, randomly shuffle it and split 67/33
+def getFiles(emotion): 
+    files = gb.glob("final_dataset\\%s\\*" %emotion)
     random.shuffle(files)
-    training = files[:int(len(files) * 0.50)]  # get first 67% of file list
-    prediction = files[-int(len(files) * 0.50):]  # get last 33% of file list
+    training = files[:int(len(files)*0.67)] #get first 67% of file list
+    prediction = files[-int(len(files)*0.33):] #get last 33% of file list
     return training, prediction
-
-
 def makeTrainingAndValidationSet():
     training_data = []
     training_labels = []
@@ -26,29 +21,26 @@ def makeTrainingAndValidationSet():
     prediction_labels = []
     for emotion in emojis:
         training, prediction = getFiles(emotion)
-        # Append data to training and prediction list, and generate labels 0-7
+        #Append data to training and prediction list, and generate labels 0-7
         for item in training:
-            image = cv2.imread(item)  # open image
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert to grayscale
-            training_data.append(gray)  # append image array to training data list
+            image = cv2.imread(item) #open image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert to grayscale
+            training_data.append(gray) #append image array to training data list
             training_labels.append(emojis.index(emotion))
-
-        for item in prediction:  # repeat above process for prediction set
+    
+        for item in prediction: #repeat above process for prediction set
             image = cv2.imread(item)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             prediction_data.append(gray)
             prediction_labels.append(emojis.index(emotion))
     return training_data, training_labels, prediction_data, prediction_labels
-
-
-def runClassifier(count):
+def runClassifier():
     training_data, training_labels, prediction_data, prediction_labels = makeTrainingAndValidationSet()
-
-    print ("training fisher face classifier suing the training data")
-    print ("size of training set is:", len(training_labels), "images")
+    
+    print "training fisher face classifier suing the training data"
+    print "size of training set is:", len(training_labels), "images"
     fisher_face.train(training_data, np.asarray(training_labels))
-    fisher_face.write("training" + str(count) + ".yml")
-    print ("classification prediction")
+    print "classification prediction"
     counter = 0
     right = 0
     wrong = 0
@@ -60,13 +52,11 @@ def runClassifier(count):
         else:
             wrong += 1
             counter += 1
-    return ((100 * right) / (right + wrong))
-
-
-# Now run the classifier
+    return ((100*right)/(right + wrong))
+#Now run the classifier
 metascore = []
-for i in range(0, 10):
-    right = runClassifier(i)
-    print ("got", right, "percent right!")
+for i in range(0,10):
+    right = runClassifier()
+    print "got", right, "percent right!"
     metascore.append(right)
-print ("\n\nend score:", np.mean(metascore), "percent right!")
+print "\n\nend score:", np.mean(metascore), "percent right!"
